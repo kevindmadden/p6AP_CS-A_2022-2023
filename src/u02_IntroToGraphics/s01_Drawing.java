6b07637ec9ff30d38239db44c342d249496bc33f
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-09-20 12:25:24 -0500

    Update s01_Drawing.java

--- src/u02_IntroToGraphics/s01_Drawing.java
+++ src/u02_IntroToGraphics/s01_Drawing.java
@@ -1,10 +1,13 @@
 package u02_IntroToGraphics;
 
+import java.awt.*;
+
 public class s01_Drawing {
     public static void main(String[] args) {
 
         StdDraw.setPenRadius(0.05); //0.05 is a parameter
         StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.setPenColor(new Color(134, 255, 97) );
         StdDraw.point(0.5, 0.5); //0.5 and 0.5 are parameters
         StdDraw.setPenColor(StdDraw.MAGENTA);
         StdDraw.line(0.2, 0.2, 0.8, 0.2);
