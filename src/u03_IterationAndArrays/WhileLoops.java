cb352ef4273d13d940e41125569bea41b4bf270a
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-10-04 12:43:47 -0500

    .

--- src/u03_IterationAndArrays/WhileLoops.java
+++
@@ -1,7 +0,0 @@
-package u03_IterationAndArrays;
-
-public class WhileLoops {
-    public static void main(String[] args){
-
-    }
-}
---
+++ src/u04_IterationAndArrays/s01_forLoops.java
@@ -0,0 +1,37 @@
+package u04_IterationAndArrays;
+
+public class s01_forLoops {
+    public static void main(String[] args){
+
+        //1. Creating the variable i and storing 0.
+        //2. End condition. As long as i<=10, it will run the code in the loop.
+        //3. i++ -This code runs once at the end of every run-through
+
+        for(int i=0; i<=10; i++){
+            System.out.print("o");
+        }
+
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+
+        //
+        // Creates and stores 0 in i
+        // check condition
+           // Is i<=10? TRUE
+        // Prints out o
+        // Once it hits }, then i++.
+        // Before: i=0.
+        // i++
+        // After: i=1.
+        // Check condition
+            // Is i<=10? TRUE
+        // Prints out o
+        // Once it hits }, then i++.
+        // Before: i=1
+        // i++
+        // After: i=2
+
+
+    }
+}
