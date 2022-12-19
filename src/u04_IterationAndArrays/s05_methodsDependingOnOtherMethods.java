268382497a06513ec7b55a454f9719502b02a071
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-12-19 14:16:28 -0600

    Update s05_methodsDependingOnOtherMethods.java

--- src/u04_IterationAndArrays/s05_methodsDependingOnOtherMethods.java
+++ src/u04_IterationAndArrays/s05_methodsDependingOnOtherMethods.java
@@ -5,7 +5,7 @@ public class s05_methodsDependingOnOtherMethods {
 
     }
 
-    //Replace all e's with a's
+    //Replace all a's with e's
     public static String replaceAlle(String phrase){
         String temp = "";
         for(int i=0; i<phrase.length(); i++){
@@ -18,7 +18,7 @@ public class s05_methodsDependingOnOtherMethods {
         return temp;
     }
 
-    //If after replacing all e's with a's, return true if the word "bed" is anywhere in the string.
+    //If after replacing all a's with e's, return true if the word "bed" is anywhere in the string.
     public static boolean isBed(String phrase){
         String temp = replaceAlle(phrase);
         for(int i=0; i<temp.length()-2; i++){
