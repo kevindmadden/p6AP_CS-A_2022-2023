d804a3eb1ad649b88582503af2dbd981ab877941
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2023-01-25 13:06:15 -0600

    Game ideas

--- src/u04_IterationAndArrays/s03_NestedForLoops.java
+++ src/u04_IterationAndArrays/s03_NestedForLoops.java
@@ -1,6 +1,38 @@
 package u04_IterationAndArrays;
 
 public class s03_NestedForLoops{
+    /*
+    Chess
+    Checkers (Make competing AI's at the end)
+    Slot Machine
+    *2048
+    Snake
+    Payday
+    Uno
+    Space invaders
+    Pac-man
+    Breakout
+    A game
+    Solitare
+    Tetris
+    Random Grid Generator - What do we do on grid?
+    Racing game where different tracks stored in grid
+    Candy Crush like game
+    Bridge builder
+    Mindless clicker game
+    Sheep herder
+    Minesweeper
+    Farming Simulator
+    Rhythm game
+    Angry birds - store info about birds on right side in grid
+    Terraria like game
+
+
+
+    Conway's game of life
+     */
+
+
     public static void main(String[] args){
         String[] desserts = {"ice cream","cookies","brownies","doughnuts","popsicles"};
         String[] fruits = {"strawberry", "cherry", "grape", "prune", "lemon"};
